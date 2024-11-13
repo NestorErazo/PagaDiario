a5f15994af76c87b50fd377b5a1b55fa1e8289f8
njerazo <njerazo@gmail.com>
2024-11-12 19:08:33 -0500

    actualizacion Logout

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,19 @@
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Menu.css'; // Importa el archivo CSS para estilizar el menú
-import { FaUser  ,FaSolarPanel     } from "react-icons/fa"; // Importar íconos
+import { FaUser, FaSolarPanel } from "react-icons/fa"; // Importar íconos
 import { FcMoneyTransfer } from "react-icons/fc";
 
 const Menu = () => {
+    const navigate = useNavigate();
+
+    // Función de logout
+    const handleLogout = () => {
+        // Aquí puedes agregar la lógica para limpiar los datos de sesión
+        localStorage.removeItem('token'); // Ejemplo para limpiar el token de sesión
+        navigate('/'); // Redirige a la página de inicio de sesión
+    };
+
     return (
         <div className="menu-container">
             <h2>Menú de la Aplicación</h2>
@@ -13,26 +21,28 @@ const Menu = () => {
                 <ul>
                     <li>
                         <Link to="/UserForm">Crear Usuario</Link>
-                          <FaUser className='icon' />
+                        <FaUser className='icon' />
                     </li>
                     <li>
                         <Link to="/CreditForm">Gestión de Préstamos</Link>
-                        <FcMoneyTransfer   />
+                        <FcMoneyTransfer />
                     </li>
                     <li>
-                        <Link to="/CreditDetails">Validar de Prestamos</Link>
-                        <FaSolarPanel      className='icon'/>
+                        <Link to="/CreditDetails">Validar Préstamos</Link>
+                        <FaSolarPanel className='icon' />
                     </li>
                     <li>
                         <Link to="/Manager">Panel Manager</Link>
-                        <FaSolarPanel      className='icon'/>
+                        <FaSolarPanel className='icon' />
                     </li>
-                    
                 </ul>
+                {/* Botón de logout */}
+                <button onClick={handleLogout} className="logout-button">
+                    Logout
+                </button>
             </nav>
         </div>
     );
-    
 };
 
 export default Menu;
